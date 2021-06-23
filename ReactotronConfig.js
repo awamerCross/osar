affaf90c066a0a7c875b52a7dd85682ec5d77c60
Mahmoud Elbatal <mahmoudcse@gmail.com>
2021-06-23 11:08:35 +0200

    Activate Code when Register

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,9 +1,13 @@
-import Reactotron from 'reactotron-react-native';
-import { reactotronRedux } from 'reactotron-redux';
+import Reactotron from "reactotron-react-native";
+import { reactotronRedux } from "reactotron-redux";
 
+const reactotron = Reactotron.configure({
+  name: "harby",
+  host: "10.0.0.25",
+  port: 9090,
+})
+  .use(reactotronRedux())
+  .useReactNative()
+  .connect();
 
-const reactotron = Reactotron.configure({ name: 'harby', host : '192.168.137.176', port: 9090 })
-    .use(reactotronRedux()).useReactNative()
-    .connect();
-
-export default reactotron
+export default reactotron;
